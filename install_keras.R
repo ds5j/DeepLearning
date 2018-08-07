d85a548f7e9c8727dc464dc4efe7c8b1b5b9021d
ds5j <ds5j@virginia.edu>
2018-08-07 16:00:13 +0000

    add file

---
+++ install_keras.R
@@ -0,0 +1,11 @@
+install.packages('here')
+install.packages('keras')
+install.packages('tidyverse')
+
+library(keras)
+#cpu version
+install_keras()
+
+
+#git config --global user.email "ds5j@virginia.edu"
+#git config --global user.name "ds5j"
