22748d2734aafa2895d97ca41ee8df5d5cffd1b8
ds5j <ds5j@virginia.edu>
2018-08-21 16:21:42 +0000

    latest

--- approx.R
+++ approx.R
@@ -30,11 +30,25 @@ y_function=function(x,a=-.25,b=.25)
 }
 train_target=y_function(train_data)
 
-test_data=matrix(seq(0,6.28,6.28/100))
+test_data=matrix(seq(0,pi*3,pi*3/100))
 test_target=y_function(test_data,0,0)
 
-
-
+############## replace train and test  ########################## ############## ############## 
+X1 = seq(0,pi,length.out = 7)[c(-1,-7)]
+X2 = seq(pi,2*pi,length.out=15)
+Y1 = -cos(X1)+runif(length(X1),-.25,.25)
+Y2 = cos(3*(X2-pi))+runif(length(X2),-.25,.25)
+X = c(X1,X2)
+dim(X)=c(length(X),1)
+Y = c(Y1,Y2)
+dim(Y)=c(length(Y),1)
+x_grid=seq(0,2*pi,length.out = 99)
+y_noiseless=c(-cos(seq(0,pi,length.out=50))[-1],cos(3*(seq(pi,2*pi,length.out = 50)-pi)))
+############## ############## ############## ############## ############## ############## 
+train_data=X
+train_target=Y
+test_data=x_grid
+test_target=y_noiseless
 
 ## get gradients
 get_grad=function(model,train_data)
@@ -54,16 +68,19 @@ predictions=vector(mode="list")
 models=vector(mode="list")
 
 #train_data=normalize(train_data)
-num_hidden_units=4
-lr0=.5/num_hidden_units
+num_hidden_units=40
+lr0=ifelse(num_hidden_units>4,.01,.07)
 #without train dim it errors on summary
-l2reg=.00008
+l2reg=.00001
 input_layer <- layer_input(shape = 1, name = 'input')
-output_layer <- input_layer %>% layer_dense(units = num_hidden_units, activation = 'tanh',kernel_regularizer = regularizer_l2(l2reg))  %>% layer_dense(units = 1,,kernel_regularizer = regularizer_l2(l2reg)) 
+output_layer <- input_layer %>% layer_dense(units = num_hidden_units, activation = 'tanh',kernel_regularizer = regularizer_l2(l2reg),bias_initializer=initializer_random_uniform(minval = -0.1, maxval = 0.1, seed = 104), kernel_initializer=initializer_random_normal(mean=0,stddev=.1, seed = 104))  %>% layer_dense(units = 1,,kernel_regularizer = regularizer_l2(l2reg),,bias_initializer=initializer_random_uniform(minval = -0.1, maxval = 0.1, seed = 104), kernel_initializer=initializer_random_normal(mean=0,stddev=.1, seed = 104)) 
+
+
+
 model_simple <- keras_model(inputs = input_layer, outputs = output_layer )
 
 #,clipnorm=1,,clipvalue=1
-opt <- optimizer_sgd(lr = lr0)
+opt <- optimizer_sgd(lr = lr0,momentum=.9)
 
 model_simple %>% compile(
   optimizer = opt, 
@@ -71,27 +88,13 @@ model_simple %>% compile(
   metrics = c("mae")
 )
 
-lr_schedule <- function(epoch,lr) {  lr0/(1+(epoch/5)) }
-lr_reducer <- callback_learning_rate_scheduler(lr_schedule)
-
-# define custom callback class
-LossHistory <- R6::R6Class("LossHistory",
-                           inherit = KerasCallback,
-                           
-                           public = list(
-                             
-                             losses = NULL,
-                             
-                             on_epoch_end = function(epoch, logs = list()) {
-                               self$losses <- c(self$losses, logs[["loss"]])
-                               print(paste('losses:',self$losses[length(self$losses)]))
-                             }
-                           ))
-history <- LossHistory$new()
-
 
 epochs=20000
 wgts=NA
+
+lr_schedule <- function(epoch,lr) {  lr0/(1+(epoch/epochs)) }
+lr_reducer <- callback_learning_rate_scheduler(lr_schedule)
+
 summary(model_simple)
 #,callback_model_checkpoint("checkpoints.h5")
 hist_approx<-model_simple %>% fit(train_data, train_target,
@@ -99,18 +102,19 @@ hist_approx<-model_simple %>% fit(train_data, train_target,
 #              ,callback_lambda(on_epoch_begin=(print(paste(get_weights(model_simple)))))
 #              ,callback_reduce_lr_on_plateau, #,list(lr_reducer)
 ,callbacks= list(callback_lambda( on_epoch_end = function(epoch, logs=list()) {
-  cat("Epoch Begin\n")
-  print(paste('epoch:',epoch))
-  wgts=get_weights(model_simple)
-  print(paste('wgt norm:',sum(unlist(wgts)^2), 'gradient norm:',get_grad(model_simple,train_data),'loss:',logs[["loss"]]))
-
+    if (epoch %% 1000==0)
+    {
+     cat("Epoch End\n")
+     print(paste('epoch:',epoch))
+     wgts=get_weights(model_simple)
+     print(paste('wgt norm:',sum(unlist(wgts)^2), 'gradient norm:',get_grad(model_simple,train_data),'loss:',logs[["loss"]]))
+  }
   
-  #print(paste('losses:',logs[["loss"]][length(logs[["loss"]])]))
 }
-))
+),callback_terminate_on_naan(),lr_reducer,callback_reduce_lr_on_plateau(monitor = "loss", factor = 0.5)  
 
 )
-              
+)
 
 plot(hist_approx)
 print(hist_approx)
